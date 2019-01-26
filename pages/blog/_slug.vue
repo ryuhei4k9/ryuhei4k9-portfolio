8b88d2a915758cfb4e8518c5204635626fefc66e
ryuhei4k9 <ryuhei.4k9@gmail.com>
2019-01-27 01:05:23 +0900

    一旦開ける

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">{{ post.title }}</h1>
+      <h2 class="subtitle">{{ $date_format(post.createdAt, "YYYY/MM/DD hh:mm:ss") }}</h2>
+      <div class="content" v-html="$md.render(post.content || '')"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import post from '~/apollo/queries/post'
+
+export default {
+  head: {
+    title: 'Blog'
+  },
+  data () {
+    return {
+      post: {}
+    }
+  },
+  apollo: {
+    post: {
+      query: post,
+      variables () {
+        return {
+          slug: this.$route.params.slug
+        }
+      }
+    }
+  }
+}
+</script>
