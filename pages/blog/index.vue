3446f7234986504d1fd8ef6aced850906192b8d3
ryuhei4k9 <ryuhei.4k9@gmail.com>
2019-02-02 23:35:12 +0900

    pagesにうつす

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <posts/>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <section class="section">
+        <div class="container">
+          <h1 class="title"><nuxt-link :to="`/blog/${ post.slug }`">{{ post.title }}</nuxt-link></h1>
+          <h2 class="subtitle">{{ $date_format(post.createdAt, "YYYY/MM/DD hh:mm:ss") }}</h2>
+          <!-- <div class="content" v-html="$md.render(post.content)"></div> -->
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-import Posts from '~/components/Posts'
+import posts from '~/apollo/queries/posts'
 
 export default {
   head: {
     title: 'Blog'
   },
-  components: {
-    Posts
+  data () {
+    return {
+      posts: {}
+    }
+  },
+  apollo: {
+    posts: {
+      query: posts,
+      prefetch: true
+    }
   }
 }
 </script>
